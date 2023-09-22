e7f93088c4a5b5b27fab32cd9972b397f0ccdb96
optimisme <optimisme@gmail.com>
2023-09-22 07:49:57 +0200

    English comments

--- scripts/svgelement.js
+++ scripts/svgelement.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', svgElementConvertImgToSvg);
 function svgElementConvertImgToSvg() {
-    // Seleccioneu totes les imatges SVG de la pàgina que estiguin dins d'un element <a>
+    // Get all site images
     const images = document.querySelectorAll('a > img[src$=".svg"]');
 
     images.forEach(img => {
@@ -9,18 +9,18 @@ function svgElementConvertImgToSvg() {
         fetch(img.src)
             .then(response => response.text())
             .then(data => {
-                // Creem un contenidor per parsejar l'SVG
+                // Create the new SVG element
                 const parser = new DOMParser();
                 const parsedSvg = parser.parseFromString(data, 'image/svg+xml');
                 const svgElement = parsedSvg.documentElement;
 
-                // Eliminem qualsevol element <script> dins de l'SVG
+                // Remove all <script> tags
                 const scripts = svgElement.querySelectorAll('script');
                 scripts.forEach(script => {
                     script.parentNode.removeChild(script);
                 });
 
-                // Copiem els atributs de l'element <img> a l'element <svg>
+                // Copy all attributes from the <img> to the <svg>
                 for (let i = 0; i < img.attributes.length; i++) {
                     const attr = img.attributes[i];
                     if (attr.name !== 'src') { // Evitem copiar l'atribut 'src'
@@ -28,14 +28,14 @@ function svgElementConvertImgToSvg() {
                     }
                 }
 
-                // Establim l'estil de l'SVG per ocupar tot l'espai disponible
+                // Set the SVG element styles
                 svgElement.style.width = "100%";
                 svgElement.style.maxWidth = "100%";
                 svgElement.style.height = "auto";
                 svgElement.style.display = "block";
                 svgElement.setAttribute('preserveAspectRatio', 'xMidYMid meet');
 
-                // Reemplaça l'element <a> amb l'element <svg>
+                // Replace the <img> with the <svg>
                 parentAnchor.replaceWith(svgElement);
             })
             .catch(error => {
